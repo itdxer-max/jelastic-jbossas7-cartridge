abf8baafc9ecfd959fc174fbc8ad480427e9136f
Dmitry Zubelevich <dz@jelastic.com>
2015-04-27 18:25:15 +0300

    initial commit

---
+++ jelastic/scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WGET=$(which wget);
+
+function _deploy(){
+     [ "x${context}" == "xroot" ] && context="ROOT";
+     [ -f "${WEBROOT}/${context}.war" ] && rm -f ${WEBROOT}/${context}.war;
+     [ -f "${WEBROOT}/${context}.war.undeployed" ] && mv ${WEBROOT}/${context}.war.undeployed ${WEBROOT}/${context}.war.deployed
+     $WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
+}
+
+function _undeploy(){
+     [ "x${context}" == "xroot" ] && context="ROOT";
+     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f ${WEBROOT}/${context}.war || return 0;
+}
