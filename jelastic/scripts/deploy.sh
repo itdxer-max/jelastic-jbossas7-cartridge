66591674d83c8f50c91ae7eeaa22ff105436c24e
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2015-05-08 10:56:10 +0300

    JE-21201 [Jboss 7.1]: Troubles with deploied files -> Failed to start service

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -3,6 +3,11 @@
 WGET=$(which wget);
 
 function _deploy(){
+     local config="${OPENSHIFT_JBOSSAS_DIR}/versions/${Version}/standalone/configuration/standalone.xml"
+     [ -f "$config" ] && grep -q "enable-welcome-root=\"true\"" "$config"  &&  { sed -i 's/enable-welcome-root=\"true\"/enable-welcome-root=\"false\"/g' "$config";
+             SERVICE=$(which service);
+             $SERVICE cartridge restart; ### require for apply new standalone.xml config
+     }     
      [ "x${context}" == "xroot" ] && context="ROOT";
      [ -f "${WEBROOT}/${context}.war" ] && rm -f ${WEBROOT}/${context}.war;
      [ -f "${WEBROOT}/${context}.war.undeployed" ] && mv ${WEBROOT}/${context}.war.undeployed ${WEBROOT}/${context}.war.deployed
